79c3cdaa58af1d85e82652b39e001c9a6cf4af8a
lgrossi <lucas.ggrossi@gmail.com>
2016-11-17 22:44:31 -0200

    Quick-sort implementation.

--- sort.c
+++ sort.c
@@ -28,10 +28,34 @@ void shellSort(TEAM* array, int elements) {
    }
 }
 
-void quickSort(TEAM teams[], int len, int size) {
-    qsort(teams, len, size, sort_cmp);
+void particao (int Esq, int Dir, int *i, int *j, TEAM *A) {
+        TEAM x, w;
+        *i = Esq; *j = Dir;
+        x = A[(*i + *j)/2]; /* obtem o pivo x */
+    do {
+        while (sort_cmp(&x, &A[*i]) > 0) (*i)++;
+        while (sort_cmp(&x, &A[*j]) < 0) (*j)--;
+        if (*i <= *j ) {
+            w = A[*i];
+            A[*i] = A[*j];
+            A[*j] = w;
+            (*i)++;
+            (*j)--;
+        }
+    } while (*i <= *j);
 }
 
+void ordena(int Esq, int Dir , TEAM *A)
+{
+    int i, j;
+    particao (Esq, Dir, &i, &j, A);
+    if (Esq < j) ordena(Esq, j, A);
+    if (i < Dir) ordena(i, Dir, A);
+}
+
+void quickSort(TEAM teams[], int len, int size) {
+    ordena(0, len-1, teams);
+}
 
 /*
 ** Comparador dos algorítimos de sorting.
